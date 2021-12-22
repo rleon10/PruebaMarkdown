7bf81dfaf7c707433e139a018798e18c1d76b172
leonvil <leonvil@mapfre.net>
2021-12-22 18:44:54 +0100

    update python script

---
+++ python.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+import os
+
+arr = os.listdir()
+
+for file in arr: 
+    print(file)
---
+++ script.sh
@@ -0,0 +1,9 @@
+#!/usr/local/bin/
+
+echo "Converting to HTML..."
+
+echo $pwd
+
+echo "HTML built"
+echo "Build complete!"
+
